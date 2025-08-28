060beabd1c8814459e4795a1c7f7ae62c23f4493
Elleniette1 <ellemin.f@gmail.com>
2025-08-28 21:26:45 +0800

    List of Malaysian Towns added

--- petro-web-scraping/utils.py
+++ petro-web-scraping/utils.py
@@ -1,17 +1,30 @@
 states = ["Federal Territory of Kuala Lumpur", "Putrajaya","Labuan Federal Territory",
-          "Selangor", "Subang Jaya", "Klang", "Shah Alam", "Petaling Jaya", "Ampang", "Cheras", "Kajang", "Selayang", "Rawang", "Taman Greenwood", "Semenyih", "Banting", "Balakong", "Gombak Setia", "Kuala Selangor", "Serendah", "Bukit Beruntung", "Pengkalan Kundang", "Jenjarom", "Sungai Besar", "Batu Arang", "Tanjung Sepat", "Kuang", "Kuala Kubu Baharu", "Batang Berjuntai", "Bandar Baru Salak Tinggi", "Sekinchan", "Sabak", "Tanjung Karang", "Beranang", "Sungai Pelek",
-          "Johor", "Johor Bahru", "Tebrau", "Pasir Gudang", "Bukit Indah", "Kulai","Kluang","Batu Pahat","Muar","Ulu Tiram","Senai","Segamat","Kulai","Kola Tinggi","Pontian Kecil","Tangkak","Bukit Bakri","Yong Peng","Pekan Nenas","Labis","Mersing","Simpang Renggam","Parit Raja","Kelapa Sawit","Buloh Kasap","Chaah"
-          "Melaka", "Bandaraya Melaka, Melaka", "Bukit Baru", "Ayer Keroh","Klebang","Masjid Tanah","Sungai Udang","Batu Berendam","Alor Gajah","Bukit Rambai","Ayer Molek","Bemban","Kuala Sungai Baru","Pulau Sebang",
-          "Negeri Sembilan", "Seremban","Port Dickson","Nilai","Bahau","Tampin","Kuala Pilah",
+          "Selangor", 
+          "Subang Jaya, Selangor", "Klang, Selangor", "Shah Alam, Selangor", "Petaling Jaya, Selangor", "Ampang, Selangor", "Cheras, Selangor", "Kajang, Selangor", "Selayang, Selangor", "Rawang, Selangor", "Taman Greenwood, Selangor", "Semenyih, Selangor", "Banting, Selangor", "Balakong, Selangor", "Gombak Setia, Selangor", "Kuala Selangor, Selangor", "Serendah, Selangor", "Bukit Beruntung, Selangor", "Pengkalan Kundang, Selangor", "Jenjarom, Selangor", "Sungai Besar, Selangor", "Batu Arang, Selangor", "Tanjung Sepat, Selangor", "Kuang, Selangor", "Kuala Kubu Baharu, Selangor", "Batang Berjuntai, Selangor", "Bandar Baru Salak Tinggi, Selangor", "Sekinchan, Selangor", "Sabak, Selangor", "Tanjung Karang, Selangor", "Beranang, Selangor", "Sungai Pelek, Selangor",
+          "Johor", 
+          "Johor Bahru, Johor", "Tebrau, Johor", "Pasir Gudang, Johor", "Bukit Indah, Johor", "Kulai, Johor","Kluang, Johor","Batu Pahat, Johor","Muar, Johor","Ulu Tiram, Johor","Senai, Johor","Segamat, Johor","Kulai, Johor","Kola Tinggi, Johor","Pontian Kecil, Johor","Tangkak, Johor","Bukit Bakri, Johor","Yong Peng, Johor","Pekan Nenas, Johor","Labis, Johor","Mersing, Johor","Simpang Renggam, Johor","Parit Raja, Johor","Kelapa Sawit, Johor","Buloh Kasap, Johor","Chaah, Johor"
+          "Melaka", 
+          "Bandaraya Melaka, Melaka", "Bukit Baru, Melaka", "Ayer Keroh, Melaka","Klebang, Melaka","Masjid Tanah, Melaka","Sungai Udang, Melaka","Batu Berendam, Melaka","Alor Gajah, Melaka","Bukit Rambai, Melaka","Ayer Molek, Melaka","Bemban, Melaka","Kuala Sungai Baru, Melaka","Pulau Sebang, Melaka",
+          "Negeri Sembilan", 
+          "Seremban, Negeri Sembilan","Port Dickson, Negeri Sembilan","Nilai, Negeri Sembilan","Bahau, Negeri Sembilan","Tampin, Negeri Sembilan","Kuala Pilah, Negeri Sembilan","Rasah, Negeri Sembilan","Senawang, Negeri Sembilan", 
           "Pahang", 
+          "Kuantan, Pahang", "Temerloh, Pahang", "Bentong, Pahang","Mentakab, Pahang","Raub, Pahang","Jerantut, Pahang","Pekan, Pahang","Kuala Lipis, Pahang","Bandar Jengka, Pahang","Bukit Tinggi, Pahang",
           "Terengganu", 
+          "Kuala Terengganu, Terengganu","Chukai, Terengganu","Dungun, Terengganu","Kerteh, Terengganu","Kuala Berang, Terengganu","Marang, Terengganu","Paka, Terengganu","Jerteh, Terengganu","Bukit Besi, Terengganu", 
           "Kelantan", 
+          "Kota Bharu, Kelantan","Pengkalan Kubor, Kelantan","Tanah Merah, Kelantan","Peringat, Kelantan","Wakaf Bharu, Kelantan","Kadok, Kelantan","Pasir Mas, Kelantan","Gua Musang, Kelantan","Kuala Krai, Kelantan","Tumpat, Kelantan","Bachok, Kelantan", 
           "Perak", 
+          "Ipoh, Perak", "Taiping, Perak", "Sitiawan, Perak","Simpang Empat, Perak","Teluk Intan, Perak","Batu Gajah, Perak","Lumut, Perak","Kampung Koh, Perak",'Kuala Kangsar, Perak','Sungai Siput, Perak','Tapah, Perak','Bidor, Perak','Parit Buntar, Perak','Ayer Tawar, Perak','Bagan Serai, Perak','Tanjung Malim, Perak','Lawan Kuda Baharu, Perak','Pantai Remis, Perak','Kampar, Perak','Kampung Gajah, Perak',
           "Perlis", 
+          'Kangar, Perlis','Arau, Perlis','Padang Besar, Perlis',
           "Kedah", 
-          "Penang",
+          'Sungai Petani, Kedah','Alor Setar, Kedah','Kulim, Kedah','Jitra, Kedah','Baling, Kedah','Pendang, Kedah','Langkawi, Kedah','Yan, Kedah','Sik, Kedah','Kuala Nerang, Kedah','Pokok Sena, Kedah','Bandar Baharu, Kedah','Kubang Pasu, Kedah',
+          "Pulau Pinang", 
+          'Bukit Mertajam, Pulau Pinang','Georgetown, Pulau Pinang','Sungai Ara, Pulau Pinang','Gelugor, Pulau Pinang','Air Itam, Pulau Pinang','Butterworth, Pulau Pinang','Perai, Pulau Pinang','Nibong Tebal, Pulau Pinang','Permatang Pauh, Pulau Pinang','Tanjung Tokong, Pulau Pinang','Kepala Batas, Pulau Pinang','Tanjung Bungah, Pulau Pinang','Juru, Pulau Pinang',
           "Sarawak Malaysia", 
-          "Sabah Malaysia"
+          'Kota Kinabalu, Sarawak','Sandakan, Sarawak','Tawau, Sarawak','Lahad Datu, Sarawak','Keningau, Sarawak','Putatan, Sarawak','Donggongon, Sarawak','Semporna, Sarawak','Kudat, Sarawak','Kunak, Sarawak','Papar, Sarawak','Ranau, Sarawak','Beaufort, Sarawak','Kinarut, Sarawak','Kota Belud, Sarawak',
+          "Sabah Malaysia",
+          'Kuching, Sabah','Miri, Sabah','Sibu, Sabah','Bintulu, Sabah','Limbang, Sabah','Sarikei, Sabah','Sri Aman, Sabah','Kapit, Sabah','Kota Samarahan, Sabah'
 ]
 
 categories = {"segarmesra":"//*[@id='pnl-filter']/div/div/div[1]/div/div[3]/div/div/div/div[2]/div/div/div/div/div[1]/ul/li[3]/label/span[1]/img",
