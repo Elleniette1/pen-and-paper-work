1aee31bdd92cd840b5db8904955b43913799cb27
Elleniette1 <ellemin.f@gmail.com>
2025-08-29 18:25:49 +0800

    2/4 collected

--- petro-web-scraping/main.py
+++ petro-web-scraping/main.py
@@ -19,6 +19,8 @@ options.add_argument("--log-level=3")
 driver = webdriver.Chrome(service=service, options=options)
 action = ActionBuilder(driver)
 driver.get("https://www.mymesra.com.my/petrol-station-finder")
+full_append = pd.read_excel("petro-web-scraping\mesraoutletsperwebsite.xlsx", engine='openpyxl',index_col=0)
+print(full_append)
 
 list_of_outlets_uncleaned = []
 WebDriverWait(driver, 15).until(
@@ -49,6 +51,8 @@ def apply_press():
     apply_button.click()
 
 def searchbar(i):
+    action.pointer_action.move_to_location(0, 0)
+    action.perform()
     try:
         WebDriverWait(driver, 15).until(
             EC.visibility_of_element_located((By.XPATH, "//*[@id='x-autocp']/div/div[1]/input"))
@@ -66,11 +70,12 @@ def searchbar(i):
     time.sleep(0.2)
     try:
         WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.XPATH, "//*[@id='htmltag']/body/div[13]/div[1]/span[2]"))
+            EC.visibility_of_element_located((By.XPATH, "//*[@id='htmltag']/body/div[13]/div[1]/span[2]"))
         )
         time.sleep(0.2)
     except:
-        pass
+        input_element.click()
+        time.sleep(0.1)
     auto_complete = driver.find_element(By.XPATH, "//*[@id='htmltag']/body/div[13]/div[1]/span[2]")
     auto_complete.click()
 
@@ -91,56 +96,58 @@ def exhaust_pages(bandar):
     except:
         pass
     j = 0
-    if driver.find_elements(By.XPATH, "//*[@id='pnl-empty']/div/div/div/div/div/div/div/div/div[4]/a/span/span[2]/div"):
-        return
-    else:
-        while (driver.find_elements(By.XPATH, "//*[@id='lst-assets']/div/div[2]/a[4]") and j < 5) or j == 0:
-            time.sleep(2)
-            for i in range(9):
-                try:
-                    nama_taman = driver.find_element(By.XPATH, f"//*[@id='lst-assets']/div/div[1]/div[{i+1}]/div/div/div/div[1]/div/div[2]/div/div[1]/div").text
-                    address_taman = driver.find_element(By.XPATH, f"//*[@id='lst-assets']/div/div[1]/div[{i+1}]/div/div/div/div[1]/div/div[2]/div/div[2]/div").text
-                except:
-                    pass
-                try:
-                    list_of_outlets_uncleaned.append([nama_taman, address_taman,bandar])
-                except:
-                    pass
+    while (driver.find_elements(By.XPATH, "//*[@id='lst-assets']/div/div[2]/a[4]") and j < 5) or j == 0:
+        time.sleep(2)
+        for i in range(9):
             try:
-                next_button = driver.find_element(By.XPATH, "//*[@id='lst-assets']/div/div[2]/a[4]")
+                nama_taman = driver.find_element(By.XPATH, f"//*[@id='lst-assets']/div/div[1]/div[{i+1}]/div/div/div/div[1]/div/div[2]/div/div[1]/div").text
+                address_taman = driver.find_element(By.XPATH, f"//*[@id='lst-assets']/div/div[1]/div[{i+1}]/div/div/div/div[1]/div/div[2]/div/div[2]/div").text
+                print(nama_taman, address_taman,bandar)
+            except:
+                print(f"No other outlets found.")
+                break
+            try:
+                list_of_outlets_uncleaned.append([nama_taman, address_taman, bandar])
             except:
                 pass
-            try:
-                next_button.click()
-            except:
-                break
-            j += 1
+        try:
+            next_button = driver.find_element(By.XPATH, "//*[@id='lst-assets']/div/div[2]/a[4]")
+        except:
+            pass
+        try:
+            next_button.click()
+        except:
+            break
+        j += 1
 t = 1
 time.sleep(5)
 print("Number of Towns: ", len(utils.states))
-for key in utils.categories:
-    driver.refresh()
-    print(f"Category: {key}\n","="*40)
-    time.sleep(2)
-    filter_press()
-    time.sleep(1)
-    option_press(utils.categories[key])
-    time.sleep(1)
-    apply_press()
-    for p in range(len(utils.states)):
-        print(f"{t}. State: ", utils.states[p])
-        searchbar(p)
-        exhaust_pages(utils.states[p])
-        time.sleep(1.5)
-        driver.find_element(By.XPATH, '//body').send_keys(Keys.CONTROL + Keys.HOME)
-        action.pointer_action.move_to_location(0, 0)
-        action.perform()
-        time.sleep(1.5)
-        t += 1
-    time.sleep(10)
+#for key in utils.categories:
+key = utils.categories['cafemesra']
+driver.refresh()
+print(f"Category: {key}\n","="*40)
+time.sleep(2)
+filter_press()
+time.sleep(1)
+option_press(key)
+time.sleep(1)
+apply_press()
+for p in range(len(utils.states)):
+    print(f"{t}. Searching State: ", utils.states[p])
+    searchbar(p)
+    exhaust_pages(utils.states[p])
+    time.sleep(1.5)
+    driver.find_element(By.XPATH, '//body').send_keys(Keys.CONTROL + Keys.HOME)
+    action.pointer_action.move_to_location(0, 10)
+    action.perform()
+    time.sleep(1.5)
+    t += 1
+a = pd.DataFrame(list_of_outlets_uncleaned).drop_duplicates(subset=[0,1]).reset_index(drop=True)
+a.to_excel(f"petro-web-scraping\mesraoutlets-up-to-cafemesra.xlsx")
 
 n=1
-full_list = pd.DataFrame(list_of_outlets_uncleaned).drop_duplicates().reset_index(drop=True)
+full_list = pd.DataFrame(list_of_outlets_uncleaned).drop_duplicates(subset=[0,1]).reset_index(drop=True)
+full_list = pd.concat([full_append, full_list]).drop_duplicates(subset=[0,1]).reset_index(drop=True)
 full_list.to_excel("petro-web-scraping\mesraoutletsperwebsite.xlsx")
 print(full_list)
 time.sleep(10)
